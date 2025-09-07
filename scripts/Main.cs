b2ff0a4b907b28d0aa33db23ba4dfc7d5291145a
ryota37 <ryota.nakahama@access-company.com>
2025-09-08 08:35:13 +0900

    Impl: preliminary card tile (3x2)

--- scripts/Main.cs
+++ scripts/Main.cs
@@ -3,4 +3,21 @@ using System;
 
 public partial class Main : Control
 {
+	[Export] public PackedScene CardScene { get; set; }
+	
+	private GridContainer _grid;
+	
+	public override void _Ready()
+	{
+		_grid = GetNode<GridContainer>("MarginContainer/Grid");
+		_grid.Columns = 3;
+		
+		int count = 6;
+		for (int i = 0; i < count; i++)
+		{
+		var card = CardScene.Instantiate<Node>();
+		_grid.AddChild(card);			
+		}
+		
+	}
 }
