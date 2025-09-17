dde97c31c09592cc51254c53150d9f7e6b79e14a
ryota37 <ryota.nakahama@access-company.com>
2025-09-17 12:34:27 +0900

    Impl: implement FaceTextures

--- scripts/Main.cs
+++ scripts/Main.cs
@@ -8,6 +8,7 @@ public partial class Main : Control
 	[Export] public int Columns { get; set; } = 4;
 	
 	[Export] public Texture2D BackTexture { get; set; }
+	[Export] public Texture2D[] FaceTextures { get; set; } = System.Array.Empty<Texture2D>();
 	
 	private GridContainer _grid;
 	
@@ -17,10 +18,15 @@ public partial class Main : Control
 		_grid.Columns = Columns;
 		
 		int total = Rows * Columns;
+		
 		for (int i = 0; i < total; i++)
 		{
 			var card = CardScene.Instantiate<Card>();
 			card.BackTexture = BackTexture;
+			
+			if (FaceTextures.Length > 0)
+				card.FaceTexture = FaceTextures[i%FaceTextures.Length];
+			
 			_grid.AddChild(card);			
 		}
 		
